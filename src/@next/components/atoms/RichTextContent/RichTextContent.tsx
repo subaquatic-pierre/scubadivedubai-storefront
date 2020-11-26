aebfc02a5d65cf68e82e8d3772f37f50598f39ac
subquatic-pierre <subquatic.pierre@gmail.com>
2020-11-26 10:31:36 +0400

    Display HTML from draftJS editor JSON object

--- src/@next/components/atoms/RichTextContent/RichTextContent.tsx
+++ src/@next/components/atoms/RichTextContent/RichTextContent.tsx
@@ -1,17 +1,34 @@
 /* eslint-disable react/no-danger */
 
-import { sanitize } from "dompurify";
-import draftToHtml from "draftjs-to-html";
+// import { sanitize } from "dompurify";
+// import draftToHtml from "draftjs-to-html";
 import React from "react";
 
 import { IProps } from "./types";
+
+interface Block {
+  key: string,
+  data: any,
+  text: string
+}
+
+const buildString = (descriptionJson: string) => {
+  const { blocks } = JSON.parse(descriptionJson)
+  const stringArr: string[] = []
+  blocks.map((obj: Block) => 
+    stringArr.push(obj.text)
+    )
+  return stringArr.join()
+}
+
+// sanitize(draftToHtml(JSON.parse(descriptionJson))
 
 export const RichTextContent: React.FC<IProps> = ({ descriptionJson }) => (
   <>
     {descriptionJson && (
       <div
         dangerouslySetInnerHTML={{
-          __html: sanitize(draftToHtml(JSON.parse(descriptionJson))),
+          __html: buildString(descriptionJson),
         }}
       />
     )}
